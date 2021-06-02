5d0037f458fc1e9dbf73ff6e5cb66afa1ca4ddcf
Matheus Silva De Oliveira <lmatheuss@indracompany.com>
2021-06-02 17:51:45 -0300

    Creating a controller

---
+++ src/events.controller.ts
@@ -0,0 +1,29 @@
+import { Controller, Delete, Get, Patch, Post } from "@nestjs/common";
+
+@Controller('/events')
+export class EventsController {
+    @Get()
+    findall() {
+
+    }
+
+    @Get()
+    findOne() {
+
+    }
+
+    @Post()
+    create() {
+
+    }
+
+    @Patch()
+    update() {
+
+    }
+
+    @Delete()
+    remove() {
+
+    }
+}

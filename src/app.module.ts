e9ef5a4ec8c17b1c75aae5a7cf69574bfcee706d
luidimso <luidi_mso@outlook.com>
2021-06-04 16:03:45 -0300

    Using environment variables to store database credencials

--- src/app.module.ts
+++ src/app.module.ts
@@ -7,18 +7,21 @@ import { Event } from './events/event.entity';
 import { EventsModule } from './events/events.module';
 import { AppItalianService } from './app.italian.service';
 import { AppDummy } from './app.dummy';
+import { ConfigModule } from '@nestjs/config';
 
 @Module({
-  imports: [TypeOrmModule.forRoot({
-    type: 'mysql',
-    host: '127.0.0.1',
-    port: 3306,
-    username: 'root',
-    password: 'example',
-    database: 'nest-events',
-    entities: [Event],
-    synchronize: true
-  }),
+  imports: [
+    ConfigModule.forRoot(),
+    TypeOrmModule.forRoot({
+      type: 'mysql',
+      host: process.env.DB_HOST,
+      port: Number(process.env.DB_PORT),
+      username: process.env.DB_USER,
+      password: process.env.DB_PASSWORD,
+      database: process.env.DB_NAME,
+      entities: [Event],
+      synchronize: true
+    }),
     EventsModule
   ],
   controllers: [AppController],

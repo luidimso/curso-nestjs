3e7c21a77487126286074ef34464811d930fd4b3
luidimso <luidi_mso@outlook.com>
2021-06-09 17:59:56 -0300

    Building a service to generate a user access token using JWT

--- src/auth/auth.module.ts
+++ src/auth/auth.module.ts
@@ -3,13 +3,24 @@ import { TypeOrmModule } from "@nestjs/typeorm";
 import { AuthController } from "./auth.controller";
 import { LocalStrategy } from "./local.strategy";
 import { User } from "./user.entity";
+import { JwtModule } from "@nestjs/jwt";
+import { AuthService } from "./auth.service";
 
 @Module({
     imports: [
-        TypeOrmModule.forFeature([User])
+        TypeOrmModule.forFeature([User]),
+        JwtModule.registerAsync({
+            useFactory: () => ({
+                secret: process.env.AUTH_SECRET,
+                signOptions: {
+                    expiresIn: '60m'
+                }
+            })
+        })
     ],
     providers: [
-        LocalStrategy
+        LocalStrategy,
+        AuthService
     ],
     controllers: [
         AuthController

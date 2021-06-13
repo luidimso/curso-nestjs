54c0b0ac9e36ddcf32166530d70a902bc04f15b2
luidimso <luidi_mso@outlook.com>
2021-06-13 19:37:59 -0300

    Using Bcrypt to hash password

--- src/auth/auth.service.ts
+++ src/auth/auth.service.ts
@@ -1,6 +1,7 @@
 import { Injectable } from "@nestjs/common";
 import { JwtService } from "@nestjs/jwt";
 import { User } from "./user.entity";
+import * as bcrypt from "bcrypt";
 
 @Injectable()
 export class AuthService {
@@ -14,6 +15,10 @@ export class AuthService {
             username: user.username,
             sub: user.id
         });
+    }
+
+    public async hashPassword(password:string): Promise<string> {
+        return await bcrypt.hash(password, 10);
     }
 
 }

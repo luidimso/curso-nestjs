b87f14e07f854cd20842c7e7f92d6f2b5c94182e
luidimso <luidi_mso@outlook.com>
2021-06-14 13:31:46 -0300

    Finishing controller to event and attendee relationship

---
+++ src/events/input/create-attendee.dto.ts
@@ -0,0 +1,6 @@
+import { IsEnum } from "class-validator";
+import { AttendeeAnswerEnum } from "../attendee.entity";
+
+export class CreateAttendeeDto {
+    @IsEnum(AttendeeAnswerEnum) answer: AttendeeAnswerEnum;
+}

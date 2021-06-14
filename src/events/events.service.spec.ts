48ec3fd913bab8b3179a5725d86d4a5da244ca2e
luidimso <luidi_mso@outlook.com>
2021-06-14 17:42:23 -0300

    Writing event service test

--- src/events/events.service.spec.ts
+++ src/events/events.service.spec.ts
@@ -2,6 +2,7 @@ import { getRepositoryToken } from "@nestjs/typeorm";
 import { Repository } from "typeorm";
 import { EventsService } from "./events.service";
 import { Test } from '@nestjs/testing';
+import { Event } from "./event.entity";
 
 describe('EventsService', () => {
     let service: EventsService;
@@ -26,5 +27,24 @@ describe('EventsService', () => {
 
         service = module.get<EventsService>(EventsService);
         repository = module.get<Repository<Event>>(getRepositoryToken(Event));
+    });
+
+    describe('updateEvent', () => {
+        it('shoud update the event', async () => {
+            const repositorySpy = jest.spyOn(repository, 'save').mockResolvedValue({
+                id: 1
+            } as Event);
+            expect(service.updateEvent(new Event({
+                id: 1
+            }), {
+                name: "New name"
+            })).resolves.toEqual({
+                id: 1
+            });
+            expect(repositorySpy).toBeCalledWith({
+                id: 1,
+                name: "New name"
+            });
+        });
     });
 });

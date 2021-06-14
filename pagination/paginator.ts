715ee8452730356bf8892ea1ba63f14339ad2fa1
luidimso <luidi_mso@outlook.com>
2021-06-13 21:46:20 -0300

    Correcting pagination class to be able to be serialized

--- pagination/paginator.ts
+++ pagination/paginator.ts
@@ -1,3 +1,4 @@
+import { Expose } from "class-transformer";
 import { SelectQueryBuilder } from "typeorm";
 
 export interface PaginateOptions {
@@ -6,12 +7,18 @@ export interface PaginateOptions {
     total?: boolean
 }
 
-export interface PaginationResult<T> {
-    first: number,
-    last: number,
-    limit: number,
-    total?: number,
-    data: T[]
+export class PaginationResult<T> {
+    constructor(
+        partial: Partial<PaginationResult<T>>
+    ) {
+        Object.assign(this, partial);
+    }
+
+    @Expose() first: number;
+    @Expose() last: number;
+    @Expose() limit: number;
+    @Expose() total?: number;
+    @Expose() data: T[];
 }
 
 export async function paginate<T>(qb: SelectQueryBuilder<T>, options: PaginateOptions = {
@@ -20,11 +27,11 @@ export async function paginate<T>(qb: SelectQueryBuilder<T>, options: PaginateOp
 }): Promise<PaginationResult<T>> {
     const offset = (options.currentPage - 1) * options.limit;
     const data = await qb.limit(options.limit).offset(offset).getMany();
-    return {
+    return new PaginationResult({
         first: offset + 1,
         last: offset + data.length,
         limit: options.limit,
         total: options.total ? await qb.getCount() : null,
         data
-    };
+    });
 }
